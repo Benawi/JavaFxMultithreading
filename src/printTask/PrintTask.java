4394b086bba66ad6e04a545512d9e65ae4efc00a
habtamu alemayehu <habtamu.alemayehu@wku.edu.et>
2022-01-23 03:14:03 +0300

    Multithreading in JavaFx

--- src/printTask/PrintTask.java
+++ src/printTask/PrintTask.java
@@ -1,6 +1,6 @@
 package printTask;
 
-// Fig. 26.3: PrintTask.java
+
 // PrintTask class sleeps for a random time from 0 to 5 seconds
 import java.util.Random;
 
@@ -40,17 +40,3 @@ public class PrintTask implements Runnable
 } // end class PrintTask
 
 
-/**************************************************************************
- * (C) Copyright 1992-2012 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
